fae27a29419eb21ce90d7e70eb5b587908a429a1
viktorvavd <99997730+viktorvavd@users.noreply.github.com>
2022-07-13 17:26:14 +0300

    Update Main.java

--- Vavdiichuk_h_2/src/test/java/Main.java
+++ Vavdiichuk_h_2/src/test/java/Main.java
@@ -1,8 +1,24 @@
 import org.junit.jupiter.api.Test;
 public class Main {
     @Test
-    void shouldHandleTypeID(){
-        int type = 1003;
-        RandomMessage randomMessage = new RandomMessage(type,25);
+    public void shouldHandleType(){
+        Product product1 = new Product(1,25,3);
+        Product product2 = new Product(2,50,9);
+        Product product3 = new Product(3,5,7);
+
+        List<Product> products = new LinkedList<>();
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        Storage storage = new Storage(products);
+
+        RandomMessage randomMessage1 = new RandomMessage(3, 10, storage);
+        RandomMessage randomMessage2 = new RandomMessage(2, 12, storage);
+        RandomMessage randomMessage3 = new RandomMessage(1, 14, storage);
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
     }
 }
