51c805685556f11587c8dc1b587a4dcf0d886c3f
viktorvavd <99997730+viktorvavd@users.noreply.github.com>
2022-07-13 17:23:48 +0300

    Update Processor.java
    final

--- Vavdiichuk_h_2/src/main/java/Processor.java
+++ Vavdiichuk_h_2/src/main/java/Processor.java
@@ -2,24 +2,58 @@ import java.nio.ByteBuffer;
 
 public class Processor extends Thread {
 
-    Message message;
+     private Message message;
+
+    private Storage storage = new Storage();
+    private int commandID;
+
     public Processor(Message message){
+        this.commandID = message.getcType();
+        this.storage = storage;
         this.message = message;
-        this.run();
+        this.start();
     }
+    public Processor(){}
 
     @Override
     public void run() {
         super.run();
-        process(this.message);
+        synchronized (storage) {
+            try {
+                while (commandID != storage.getState()) {
+                    storage.wait(100L);
+                    if(commandID != storage.getState()){
+                        if(commandID == 1 || commandID == 2 || commandID == 3){
+                            commandID++;
+                        }else{
+                            commandID = 1;
+                        }
+                    }
+                }
+                if (commandID == 1) {
+                    storage.command1();
+                } else if (commandID == 2) {
+                   storage.command2();
+                } else if (commandID == 3) {
+                    storage.command3();
+                } else if (commandID == 4)  {
+                    storage.command4();
+                }
+                storage.notify();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private void process(Message message){
+    public void process(Message message, Storage storage){
+        int id = message.getcType();
+        Processor processor = new Processor(message,storage);
         byte one = 'O';
         byte two = 'K';
         ByteBuffer messBuff = ByteBuffer.wrap(message.getMessage());
         ByteBuffer feedbackBuffer = ByteBuffer.allocate(2).put(one).put(two);
         Message feedback = new Message(messBuff.getInt(0), messBuff.getInt(4), feedbackBuffer.array());
-        Encriptor encriptor = new Encriptor(feedback);
+        Encriptor encriptor = new Encriptor(feedback, storage);
     }
 }
